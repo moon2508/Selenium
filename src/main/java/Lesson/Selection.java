91acb3ed6ccc1730f85d72a3c3971a2322cb777f
hangttp25082001 <hanghien25.8@gmail.com>
2024-08-08 23:14:25 +0700

    Selection 100%

--- src/main/java/Lesson/Selection.java
+++ src/main/java/Lesson/Selection.java
@@ -1,19 +1,22 @@
 package Lesson;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 
 
 public class Selection {
 
     public static void main(String[] args) throws InterruptedException {
         String web_url = "https://react.semantic-ui.com/maximize/dropdown-example-search-selection";
-        String searchSelect = "//@id=[search]";
-        String selectOption = ".visible.menu.transition>div>.text";
+        String searchSelect = "//input[@class='search']";
+        String selectOption = ".visible.menu.transition>.selected.item>span";
         String valueSelect = "Benin";
+        String textbox = ".divider.text";
 
         WebDriver driver = new ChromeDriver();
 
@@ -21,10 +24,15 @@ public class Selection {
         driver.get(web_url);
         //find element
         WebElement elementSel = driver.findElement(By.xpath(searchSelect));
-        WebElement options = driver.findElement(By.cssSelector(selectOption));
         elementSel.click();
-//        Select list = new Select(options);
-//        list.selectByValue(valueSelect);
+
+        WebElement options = driver.findElement(By.cssSelector(selectOption));
+        elementSel.sendKeys(valueSelect);
+        elementSel.sendKeys(Keys.ENTER);
+
+        //Verify data
+        WebElement data = driver.findElement(By.cssSelector(textbox));
+        Assert.assertEquals(data.getText(),valueSelect);
         Thread.sleep(2000);
         driver.quit();
     }
