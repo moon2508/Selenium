79fd0042dbc7ac8f18f2e4ed1a6d12af4c358441
hangttp25082001 <hanghien25.8@gmail.com>
2024-09-05 23:09:47 +0700

    update code - 05/09 bổ sung

--- src/main/java/ActualProject/Project.java
+++ src/main/java/ActualProject/Project.java
@@ -4,10 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
-import java.text.DecimalFormat;
+
 
 public class Project {
 
